50c9f9cbc900169bca870bdcafa1df2a3aa34075
Gaurav Singh <gauravnumber@gmail.com>
2025-04-07 14:44:22 +0530

    now work offline

---
+++ sw.js
@@ -0,0 +1,36 @@
+// sw.js
+const CACHE_NAME = "vent-wall-v2";
+const urlsToCache = ["./", "./index.html"];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+        )
+      )
+  );
+});
